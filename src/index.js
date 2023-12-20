bd5373941a42211150208b9db40b3f5579130401
Anna <anjanadvorna@gmail.com>
2023-12-20 16:16:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App/App';
-import { ThemeProvider } from '@emotion/react'
-import theme from 'constants/theme';
-import './index.css';
+import { ThemeProvider } from '@emotion/react';
 import { store} from './redux/store';
 import { Provider } from 'react-redux';
+import theme from 'constants/theme';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
